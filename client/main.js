c3ea4ea5be3d65531ad8111be327c9d9aa6b31b0
OmarLaham <p.omarlahham@gmail.com>
2019-01-06 20:17:30 +0200

    added Leaflet map and loaded into main tempale (Map tempalte)

--- client/main.js
+++ client/main.js
@@ -3,20 +3,82 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+//Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
-  this.counter = new ReactiveVar(0);
+//  this.counter = new ReactiveVar(0);
+//});
+
+//Template.hello.helpers({
+//  counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
+
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
+
+// on startup run resizing event
+Meteor.startup(function() {
+  $(window).resize(function() {
+    $('#map').css('height', window.innerHeight - 82 - 45);
+  });
+  $(window).resize(); // trigger resize event
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+Template.Map.onCreated(function() {
+    var self = this;
+
+    self.autorun(function() {
+
+    });
+
+    self.subscribe('markers');
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+Template.Map.rendered = function() {
+  L.Icon.Default.imagePath = '/packages/bevanhunt_leaflet/images/';
+
+  var map = L.map('map', {
+    doubleClickZoom: false
+  }).setView([0, 0], 2);
+
+  //L.TileLayer('http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png');
+  L.tileLayer.provider('Esri.WorldStreetMap').addTo(map);
+
+  map.on('dblclick', function(event) {
+    Meteor.call('insertMarker', event.latlng, function(error) {
+      console.log(error);
+    });
+  });
+
+  // add clustermarkers
+  var markers = L.markerClusterGroup();
+  map.addLayer(markers);
+
+  // var query = Markers.find();
+  // query.observe({
+  //   added: function (document) {
+  //     var marker = L.marker(document.latlng)
+  //       .on('click', function(event) {
+  //         Markers.remove({_id: document._id});
+  //       });
+  //      markers.addLayer(marker);
+  //   },
+  //   removed: function (oldDocument) {
+  //     layers = map._layers;
+  //     var key, val;
+  //     for (key in layers) {
+  //       val = layers[key];
+  //       if (val._latlng) {
+  //         if (val._latlng.lat === oldDocument.latlng.lat && val._latlng.lng === oldDocument.latlng.lng) {
+  //           markers.removeLayer(val);
+  //         }
+  //       }
+  //     }
+  //   }
+  // });
+};
--- server/main.js
+++ server/main.js
@@ -3,3 +3,19 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+Meteor.methods({
+   insertMarker: function(latlng) {
+       Markers.insert({latlng: latlng});
+       return 'done';
+   },
+   removeMarker: function(_user_id) {
+      // TODO
+   }
+});
+
+// Listen to incoming HTTP requests, can only be used on the server
+WebApp.connectHandlers.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  return next();
+});
