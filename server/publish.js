0037407af674a8680e04d800ad5030795962b71c
HIba <hiba.berghouly@medicus.ai>
2019-01-07 22:29:32 +0200

    moving the functionality to publish file

--- server/publish.js
+++ server/publish.js
@@ -1,3 +1,19 @@
-Meteor.publish("markers", function () {
-  return Markers.find();
-});
+import { Mongo } from 'meteor/mongo';
+import {Meteor} from "meteor/meteor";
+
+export const Markers = new Mongo.Collection('markers');
+
+if(Meteor.isServer){
+    Meteor.methods({
+        insertMarker: function(latlng) {
+            Markers.insert({latlng: latlng});
+            return 'done';
+        },
+        removeMarker: function(_user_id) {
+            // TODO
+        },
+        getAll:function(){
+            return Markers.find({})
+        }
+    });
+}
